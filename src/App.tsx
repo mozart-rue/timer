d494c6daa786ab7a5f4ee1686939e6f1e57cd9b8
mozart-rue <mozartrue@outlook.com>
2024-03-20 21:51:50 -0300

    feat: Initialize Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { useState } from 'react'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello Ignite</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
