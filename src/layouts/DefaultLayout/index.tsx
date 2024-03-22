d6b77f5d6c88a386aa94d43a7e3651406817d19d
mozart-rue <mozartrue@outlook.com>
2024-03-21 22:11:54 -0300

    fix: import header from index file

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,4 +1,4 @@
-import {Header} from '../../components/Header.tsx'
+import {Header} from '../../components/Header/index.tsx'
 import { Outlet } from "react-router-dom";
 import { LayoutContainer } from './styles.ts';
 
