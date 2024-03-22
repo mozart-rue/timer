516f5d29ec6195fcd5bb5a38992f42b65f27b679
mozart-rue <mozartrue@outlook.com>
2024-03-21 21:05:09 -0300

    fix: remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from 'styled-components'
 import {BrowserRouter} from 'react-router-dom'
-import { useState } from 'react'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <ThemeProvider theme={defaultTheme}>
