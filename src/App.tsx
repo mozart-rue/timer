4cb42f75eccea308d9cdb1468501b59599b4d075
mozart-rue <mozartrue@outlook.com>
2024-03-21 21:04:40 -0300

    feat: Set router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import {BrowserRouter} from 'react-router-dom'
 import { useState } from 'react'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Ignite</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
