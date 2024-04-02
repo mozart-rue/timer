0c9c2a8495b71b552715b7d84ff31ec6e7b6b8e9
mozart-rue <mozartrue@outlook.com>
2024-04-01 23:01:59 -0300

    feat: Start history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,54 @@
+import { HisotoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HisotoryContainer>
+      <h1>Meu historico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td><Status statusColor="green">Concluido</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td><Status statusColor="green">Concluido</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td><Status statusColor="green">Concluido</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td><Status statusColor="yellow">Em Andamento</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td><Status statusColor="red">Interrompido</Status></td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HisotoryContainer>
+  )
 }
